d4aaa128159490219187a9f1e0e4f1b216ce1ae8
Paul Weber <paul@iwa-tech.com>
2018-07-03 10:39:21 +0200

    Added first integration of the clustering service.

--- src/apollo.ai/ApolloAiClient.ts
+++ src/apollo.ai/ApolloAiClient.ts
@@ -9,7 +9,29 @@ export interface IAutoAbstractResponse {
   url: string;
 }
 
+export interface IClusteringResponse {
+  status: number;
+  message: string;
+  data: IClusteringArticle[][];
+}
+
+export interface IClusteringArticle {
+  id: string;
+  title: string;
+  content: string;
+  link?: string;
+  publication_date?: string;
+  image?: string;
+}
+
+export enum ClusteringLanguage {
+  en = 'en',
+  de = 'de',
+}
+
 export class ApolloAiClient {
+
+  public apolloApiEndpoint = 'https://api.apollo.ai/';
 
   constructor(protected apiKey: string) {}
 
@@ -26,7 +48,7 @@ export class ApolloAiClient {
       body.keywords = keywords.join(',');
     }
 
-    const response = await fetch('https://api.apollo.ai/autoabstract', {
+    const response = await fetch(this.apolloApiEndpoint + 'autoabstract', {
       method: 'POST',
       headers: {
         'Accept': 'application/json',
@@ -41,6 +63,26 @@ export class ApolloAiClient {
     }
 
     return await response.json();
+  }
+
+  public async clustering(articles: IClusteringArticle[], threshold = 0.8, language = ClusteringLanguage.de): Promise<IClusteringResponse> {
+
+    const url = new URL(this.apolloApiEndpoint + '/clustering');
+    url.searchParams.append('threshold', threshold.toString());
+    url.searchParams.append('language', language);
+
+    const collectionResult = await fetch(url.toString(), {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + this.apiKey,
+      },
+      body: JSON.stringify(articles),
+      timeout: 300000,
+    });
+    const clusterResult = await collectionResult.json();
+    return clusterResult;
   }
 
 }
